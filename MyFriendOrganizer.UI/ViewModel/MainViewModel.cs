916d220b5052819d5db287de06b2d07fcf1b6744
Cristóbal Tello <cristobal.tello@outlook.com>
2019-07-11 18:34:10 +0200

    Create the MainViewModel

---
+++ MyFriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MyFriendOrganizer.Model;
+using MyFriendOrganizer.UI.Data;
+using System.Collections.ObjectModel;
+
+namespace MyFriendOrganizer.UI.ViewModel
+{
+    class MainViewModel
+    {
+        IFriendDataService friendDataService;
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            this.friendDataService = friendDataService;
+        }
+
+        public void Load()
+        {
+            Friends = new ObservableCollection<Friend>(friendDataService.GetAll()); ;
+        }
+
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        private Friend selectedFriend;
+        public Friend SelectedFriend
+        {
+            get
+            {
+                return this.selectedFriend;
+            }
+            set
+            {
+                this.selectedFriend = value;
+            }
+        }
+    }
+}
