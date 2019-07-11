25edd5b892e68b7dc8703c9a84e84292999625b4
Cristóbal Tello <cristobal.tello@outlook.com>
2019-07-11 18:06:53 +0200

    Plan the User Interface Layer

---
+++ MyFriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using MyFriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace MyFriendOrganizer.UI.Data
+{
+    public class FriendDataService
+    {
+        IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Leo", LastName = "Messi" };
+            yield return new Friend { FirstName = "Cristiano", LastName = "Ronaldo" };
+            yield return new Friend { FirstName = "Carles", LastName = "Puyol" };
+            yield return new Friend { FirstName = "Santiago", LastName = "Cañizares" };
+            yield return new Friend { FirstName = "Rubén", LastName = "Pérez" };
+        }
+    }
+}
